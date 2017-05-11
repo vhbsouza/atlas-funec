d52da59393d569e347918ac1770a368252d3d71f
Victor Hugo Souza <vhbsouza@gmail.com>
2017-05-11 19:06:23 -0300

    novo serviço

--- src/providers/countries-service.ts
+++ src/providers/countries-service.ts
@@ -2,27 +2,48 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import { Observable } from 'rxjs/Rx';
 
-/*
-  Generated class for the CountriesService provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class CountriesService {
   baseUrl: string = 'https://restcountries.eu/rest/v2';
 
+  private countriesList;
+  public bookmarkedCountries: Set<Object>;
+
   constructor(public http: Http) {
     console.log('Hello CountriesService Provider');
+    this.bookmarkedCountries = new Set();
   }
 
   getCountries() {
-    return this.http.get(this.baseUrl + '/all?fields=name;alpha3Code')
+    if (this.countriesList) {
+      return Observable.of(this.countriesList);
+    } else {
+      return this.http.get(this.baseUrl + '/all?fields=name;capital;alpha3Code')
+        .map(resp => resp.json())
+        .do((data) => { this.countriesList = data; console.log("deu certo") })
+    }
+  }
+
+  getCountryInfo(alpha3Code) {
+    return this.http.get(this.baseUrl + '/alpha/' + alpha3Code)
       .map(resp => resp.json())
   }
-  getCountryInfo(code: string) {
-    return this.http.get(this.baseUrl + '/alpha/' + code)
-      .map(resp => resp.json())
+
+  bookmarkCountry(country): Promise<string> {
+    return new Promise<string>((resolve) => {
+      if (this.bookmarkedCountries.has(country)) {
+        this.bookmarkedCountries.delete(country);
+        console.log(country.name, 'removed from bookmark list');
+        resolve('removed');
+      } else {
+        this.bookmarkedCountries.add(country);
+        console.log(country.name, 'added into the bookmark list');
+        resolve('added');
+      }
+    });
+
   }
+
 }
